e1bb98d70b37d2c7a9249dc26c6af84af412a387
Orri <orrmann@gmail.com>
2024-01-11 15:12:42 +0200

    Initial Commit

---
+++ TheBaker/TheBakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheBakerApp.swift
+//  TheBaker
+//
+//  Created by Orri Arnórsson on 11.1.2024.
+//
+
+import SwiftUI
+
+@main
+struct TheBakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
